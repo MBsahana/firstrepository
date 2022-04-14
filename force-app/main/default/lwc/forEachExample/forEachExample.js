9445d135d191e7d1f0a81b1c3d4c8288dba0763a
MBsahana <sahana.mb@ceptes.com>
2022-04-14 14:55:18 +0530

    Initial Commit

---
+++ force-app/main/default/lwc/forEachExample/forEachExample.js
@@ -0,0 +1,32 @@
+import { LightningElement } from 'lwc';
+
+export default class ForEachExample extends LightningElement {
+
+    contacts = [
+
+    {
+    Id :1,
+    Name : 'Sahana',
+    Title : 'VP of Engineering',
+
+    },
+    {
+        Id :2,
+        Name : 'Shabana',
+        Title : 'VP of sales',
+    
+        },
+        {
+            Id :3,
+            Name : 'Sania',
+            Title : 'CEO',
+        
+            },
+
+
+
+
+    ];
+
+
+}
